bc2756a50210a3068829cff4d0a7343d6a0f35a5
hp <hp@LAPTOP-QISBUO97>
2022-11-08 17:49:46 +0530

    First Commit-I

---
+++ src/Basic/Demo1.java
@@ -0,0 +1,26 @@
+package Basic;
+
+public class Demo1 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Good Morning");
+		
+	}
+	
+	public static void m1()
+	{
+		
+		System.out.println("Hello");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
